c8c1a830b4288b767fd106299bfacf37fcd1f3aa
Sean Hsu <yh742@cornell.edu>
2017-11-09 21:22:44 -0500

    Initial skeleton code

---
+++ main.cc
@@ -0,0 +1,97 @@
+
+#include <unistd.h>
+
+#include <QVBoxLayout>
+#include <QApplication>
+#include <QDebug>
+
+#include "main.hh"
+
+ChatDialog::ChatDialog()
+{
+	setWindowTitle("P2Papp");
+
+	// Read-only text box where we display messages from everyone.
+	// This widget expands both horizontally and vertically.
+	textview = new QTextEdit(this);
+	textview->setReadOnly(true);
+
+	// Small text-entry box the user can enter messages.
+	// This widget normally expands only horizontally,
+	// leaving extra vertical space for the textview widget.
+	//
+	// You might change this into a read/write QTextEdit,
+	// so that the user can easily enter multi-line messages.
+	textline = new QLineEdit(this);
+
+	// Lay out the widgets to appear in the main window.
+	// For Qt widget and layout concepts see:
+	// http://doc.qt.nokia.com/4.7-snapshot/widgets-and-layouts.html
+	QVBoxLayout *layout = new QVBoxLayout();
+	layout->addWidget(textview);
+	layout->addWidget(textline);
+	setLayout(layout);
+
+	// Register a callback on the textline's returnPressed signal
+	// so that we can send the message entered by the user.
+	connect(textline, SIGNAL(returnPressed()),
+		this, SLOT(gotReturnPressed()));
+}
+
+void ChatDialog::gotReturnPressed()
+{
+	// Initially, just echo the string locally.
+	// Insert some networking code here...
+	qDebug() << "FIX: send message to other peers: " << textline->text();
+	textview->append(textline->text());
+
+	// Clear the textline to get ready for the next input message.
+	textline->clear();
+}
+
+NetSocket::NetSocket()
+{
+	// Pick a range of four UDP ports to try to allocate by default,
+	// computed based on my Unix user ID.
+	// This makes it trivial for up to four P2Papp instances per user
+	// to find each other on the same host,
+	// barring UDP port conflicts with other applications
+	// (which are quite possible).
+	// We use the range from 32768 to 49151 for this purpose.
+	myPortMin = 32768 + (getuid() % 4096)*4;
+	myPortMax = myPortMin + 3;
+}
+
+bool NetSocket::bind()
+{
+	// Try to bind to each of the range myPortMin..myPortMax in turn.
+	for (int p = myPortMin; p <= myPortMax; p++) {
+		if (QUdpSocket::bind(p)) {
+			qDebug() << "bound to UDP port " << p;
+			return true;
+		}
+	}
+
+	qDebug() << "Oops, no ports in my default range " << myPortMin
+		<< "-" << myPortMax << " available";
+	return false;
+}
+
+int main(int argc, char **argv)
+{
+	// Initialize Qt toolkit
+	QApplication app(argc,argv);
+
+	// Create an initial chat dialog window
+	ChatDialog dialog;
+	dialog.show();
+
+	// Create a UDP network socket
+	NetSocket sock;
+	if (!sock.bind())
+		exit(1);
+
+	// Enter the Qt main loop; everything else is event driven
+	return app.exec();
+}
+
---
+++ main.hh
@@ -0,0 +1,38 @@
+#ifndef P2PAPP_MAIN_HH
+#define P2PAPP_MAIN_HH
+
+#include <QDialog>
+#include <QTextEdit>
+#include <QLineEdit>
+#include <QUdpSocket>
+
+class ChatDialog : public QDialog
+{
+	Q_OBJECT
+
+public:
+	ChatDialog();
+
+public slots:
+	void gotReturnPressed();
+
+private:
+	QTextEdit *textview;
+	QLineEdit *textline;
+};
+
+class NetSocket : public QUdpSocket
+{
+	Q_OBJECT
+
+public:
+	NetSocket();
+
+	// Bind this socket to a P2Papp-specific default port.
+	bool bind();
+
+private:
+	int myPortMin, myPortMax;
+};
+
+#endif // P2PAPP_MAIN_HH
